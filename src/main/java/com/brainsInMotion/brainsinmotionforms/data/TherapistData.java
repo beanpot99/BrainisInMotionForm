97db240bda7a94c0e8d05eff5f9638b985ba2108
Grace Thompson <Grace.thompson17@aol.com>
2022-06-11 17:00:07 -0500

    add

--- src/main/java/com/brainsInMotion/brainsinmotionforms/data/TherapistData.java
+++ src/main/java/com/brainsInMotion/brainsinmotionforms/data/TherapistData.java
@@ -5,7 +5,6 @@ import com.brainsInMotion.brainsinmotionforms.models.Therapist;
 import java.util.ArrayList;
 
 public class TherapistData {
-
-
+Therapist kristiWarren = new Therapist("Kristi Warren", "MOT, OT/L", "OT", "217-891-1524", "photo URL to come later");
 
 }
